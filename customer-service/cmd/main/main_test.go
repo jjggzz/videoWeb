6539b91e994e535c41b637d3b31364253c843a5e
jjggzz <1945282561@qq.com>
2020-12-19 11:44:58 +0800

    究极折磨

--- customer-service/cmd/main/main_test.go
+++ customer-service/cmd/main/main_test.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/afex/hystrix-go/hystrix"
+	"github.com/go-kit/kit/endpoint"
 	"github.com/jjggzz/kj/discovery"
+	"github.com/jjggzz/kj/errors"
 	"github.com/jjggzz/kj/log"
 	"github.com/jjggzz/kj/track"
 	"os"
 	"testing"
-	"time"
 	"videoWeb/customer-service/proto"
 	"videoWeb/customer-service/svc/client/grpc"
 	genpb "videoWeb/generate-service/proto"
@@ -56,15 +58,33 @@ func Test_main2(t *testing.T) {
 	)
 	instancer, _ := consulDiscovery.Discovery("generate-service")
 	tracer, _ := track.BuildZipkinTracer("192.168.151.109:9411", "test2")
+
+	//对hystrix进行配置
+	//commandName:="my_endpoint"
+	//hystrix.ConfigureCommand(commandName,hystrix.CommandConfig{
+	//	Timeout:1000*3, //超时
+	//	MaxConcurrentRequests:100, //最大并发的请求数
+	//	RequestVolumeThreshold:5,//请求量阈值
+	//	SleepWindow:10000, //熔断开启多久尝试发起一次请求
+	//	ErrorPercentThreshold:1, //误差阈值百分比
+	//})
+	//breakerMw:=MyHystrix(commandName,fallback) //定义熔断器中间件
+	//
+	//server, _ := genrpc.NewLoadBalanceClient(
+	//	instancer,
+	//	breakerMw,
+	//	tracer,
+	//	logger,
+	//)
 	server, _ := genrpc.NewLoadBalanceClient(
 		instancer,
 		tracer,
 		logger,
 	)
 	sum := 0
-	for i := 0; i < 1000; i++ {
-		deadline, _ := context.WithDeadline(context.Background(), time.Now().Add(time.Second*100))
-		response, err := server.GenerateStringKey(deadline, &genpb.Empty{})
+	for i := 0; i < 100; i++ {
+		//deadline, _ := context.WithDeadline(context.Background(), time.Now().Add(time.Second*100))
+		response, err := server.GenerateStringKey(context.Background(), &genpb.Empty{})
 		if err != nil {
 			fmt.Println("error: " + err.Error())
 			sum++
@@ -73,4 +93,23 @@ func Test_main2(t *testing.T) {
 		fmt.Println(response.Id)
 	}
 	fmt.Println(sum)
+}
+
+func MyHystrix(commandName string, fallback func(error) error) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+			var resp interface{}
+			if err := hystrix.Do(commandName, func() (err error) {
+				resp, err = next(ctx, request)
+				return err
+			}, fallback); err != nil {
+				return nil, err
+			}
+			return resp, nil
+		}
+	}
+}
+
+func fallback(err error) error {
+	return errors.New(err.Error()).Append("系统正在忙")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module videoWeb
 go 1.14
 
 require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
